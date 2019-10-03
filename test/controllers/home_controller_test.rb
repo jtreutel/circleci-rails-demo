89651e61f1b4201934b8bbb45606974c2483feff
Aaron Stillwell <aaron.stillwell@circleci.com>
2019-10-03 15:26:38 +0100

    Add test assertion for button text

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,5 +4,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get ''
     assert_response :success
+    assert_select '.lead a', 'Learn more about StillCoin'
   end
 end
