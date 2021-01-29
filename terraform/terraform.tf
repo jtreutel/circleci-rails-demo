b4db80bcf813faa044c38ddb50903ce600219968
jtreutel <jennings@circleci.com>
2021-01-29 09:09:02 +0900

    terraform fmt and add random string to avoid name collision

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,14 +1,20 @@
 resource "heroku_app" "rails_app" {
-    name   = "rails-circle-demo"
-    region = "us"
+  name   = "rails-circle-demo-${random_string.random.result}"
+  region = "us"
 
-    buildpacks = [
-        "heroku/nodejs",
-        "heroku/ruby"
-    ]
+  buildpacks = [
+    "heroku/nodejs",
+    "heroku/ruby"
+  ]
 }
 
 resource "heroku_addon" "rails_database" {
-    app  = heroku_app.rails_app.name
-    plan = "heroku-postgresql:hobby-dev"
+  app  = heroku_app.rails_app.name
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "random_string" "random" {
+  length  = 5
+  upper   = false
+  special = false
 }
